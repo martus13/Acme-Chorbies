34af7c0ead294e85cc161af82651d6e3f469c5bc
pedrosr14 <p.serrano.r.5@gmail.com>
2017-04-04 04:47:24 +0200

    Populate y repositorios.

--- Item 3/Acme-Chorbies/src/main/java/converters/StringToChirpConverter.java
+++ Item 3/Acme-Chorbies/src/main/java/converters/StringToChirpConverter.java
@@ -7,6 +7,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import repositories.ChirpRepository;
 import domain.Chirp;
 
 @Component
